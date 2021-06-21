d1db0598b2e9388d8985b86f15c63d10782fc06e
fioalpha <fioalpha@gmail.com>
2021-06-21 09:09:32 -0300

    change inject url to mock server web

--- app/src/main/java/com/fioalpha/poc/githubarchitecturepoc/CustomApplication.kt
+++ app/src/main/java/com/fioalpha/poc/githubarchitecturepoc/CustomApplication.kt
@@ -8,14 +8,21 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 
 class CustomApplication: Application() {
+
+    val applicationModule = module {
+        single(qualifier = named("URL")) { "https://api.github.com/" }
+    }
+
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidLogger(Level.NONE)
             androidContext(this@CustomApplication)
-            modules(repoModule, dataModuleDi, domainModuleDi)
+            modules(applicationModule, repoModule, dataModuleDi, domainModuleDi)
         }
     }
 }
--- infra/data/src/main/java/com/fioalpha/poc/data/di/DataModuleDi.kt
+++ infra/data/src/main/java/com/fioalpha/poc/data/di/DataModuleDi.kt
@@ -5,12 +5,15 @@ import com.fioalpha.poc.data.retrofit.OkHttpClient
 import com.fioalpha.poc.data.retrofit.RetrofitService
 import com.fioalpha.poc.data.service.Service
 import com.fioalpha.poc.domain.Repository
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dataModuleDi = module {
+
     single<Service> {
         RetrofitService.createRetrofit(
-            client = OkHttpClient.create()
+            client = OkHttpClient.create(),
+            url = get(qualifier = named("URL"))
         ).create(Service::class.java)
     }
 
